e52422e96cab8f4e16477b7fda2ff436d962ca02
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-04-16 02:07:26 -0500

    first commit

---
+++ src/main/java/com/electoral/citizen_query_service/CitizenQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.electoral.citizen_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenQueryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electoral/citizen_query_service/CitizenQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electoral.citizen_query_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
